653d4fddebca33b05ad806e57672f68076d2bb7a
Emilien M <martinez.emilien@gmail.com>
2016-02-29 19:27:45 +0100

    adding AVR power status

--- services/OnkyoService.js
+++ services/OnkyoService.js
@@ -4,23 +4,31 @@ var request = require('request');
 var progress = require('request-progress');
 require('shelljs/global');
 
-function executeCmd(cmd, userId){
+function executeCmd(cmd, isAsync){
 	
 	sails.log.info('starting cmd execution');
 	// emit an event
 	ScenarioService.launcher('onkyo_command_start', cmd);
 	
 	
-	var ps = exec(cmd, {async:true});
+	var ps = exec("onkyo "+cmd, {isAsync});
 	sails.log.info('cmd executed');
-	ps.stdout.on('data', function(data) {
-			// send desktop notification (with websockets)
-				SocketService.sendDesktopMessageUser(userId, 'command_finished' , data, function(err, nbOfMsgSent){
-				 if(err) {
-					 sails.log.warn(err);
-				 }
-				});
-	});
+	
+	if(isAsync)
+	{
+		ps.stdout.on('data', function(data) {
+				// send desktop notification (with websockets)
+					SocketService.sendDesktopMessageUser(userId, 'command_finished' , data, function(err, nbOfMsgSent){
+					 if(err) {
+						 sails.log.warn(err);
+					 }
+					});
+		});
+	}
+	else
+	{
+		return ps.stdout;
+	}
 	
 }
 
@@ -33,6 +41,16 @@ module.exports = {
 	 */
 	send: function(cmd, userId){
 				
-		executeCmd(cmd, userId);
-	}	
+		executeCmd(cmd, true);
+	},
+	isAvrOn: function(){
+				
+		return executeCmd("system-power:query | egrep 'standby|on' -o", false);
+		/* return new Promise(function(resolve, reject) {
+                io.socket.post('command_finished', function (data){
+                    return resolve(data =="on");
+                });
+            });		*/
+	}
+		
 };
